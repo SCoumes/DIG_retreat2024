3035f9ab9938bf5b91d81c5ec35525ddf1cc1608
Simon Coumes <simon.coumes@ens-rennes.fr>
2024-05-27 18:57:05 +0200

    Change handling of mainlabel in graph generation Handle as a label object rather than a lit (pobably a str) Handle default None case

--- encoding_yago.py
+++ encoding_yago.py
@@ -16,21 +16,22 @@ nltk.download('omw-1.4')
 
 # Define the path to your .tsv file
 file_path_prefix = "What-Mark-Giveth"
-file_path = join(file_path_prefix,"list-yago-types.tsv")
+file_path_types = join(file_path_prefix,"list-yago-types.tsv")
 file_path_facts = join(file_path_prefix,"list-yago-facts.tsv")
-file_path = join(file_path_prefix,"yago-taxonomy.tsv")
+file_path_taxo = join(file_path_prefix,"yago-taxonomy.tsv")
 
 # Read the .tsv file into a DataFrame
-df = pd.read_csv(file_path, sep="\t", header=None)
+df = pd.read_csv(file_path_types, sep="\t", header=None)
 
 # Read the .tsv file into a DataFrame
 df_facts = pd.read_csv(file_path_facts, sep="\t", header=None)
 
 # Read the .tsv file into a DataFrame getting rid of the first 34 rows
-taxonomy = pd.read_csv(file_path, sep="\t", header=None, skiprows=34)
+taxonomy = pd.read_csv(file_path_taxo, sep="\t", header=None, skiprows=34)
 
 concepts = []
-with open('codes.csv', 'rt', encoding='utf-8') as codes:
+path_to_codes = join("utils", "codes.csv")
+with open(path_to_codes, 'rt', encoding='utf-8') as codes:
     for line in codes:
         split=line.lower().split('\n')[0].split(',')[1:]
         concepts = concepts + split
@@ -46,22 +47,23 @@ def get_main_label(word, df_yago):
     if word in df_yago[0].values:
         # get the corresponding label in column 2
         label = df_yago[df_yago[0] == word][2].values[0]
-        return [label]
+        return label # Simon : I think this did not need to be a list? It always had length 1 and was only used for its first element.
     else:
         return None
 
-def get_all_parents_from_main_label(labels, taxonomy):
+def get_all_parents_from_main_label(label, taxonomy):
     # check if the label is in the taxonomy DataFrame in column 0
-    for label in labels:
-        if label in taxonomy[0].values:
-            # get the corresponding parent in column 0
-            parents = list(taxonomy[taxonomy[0] == label][2])
-            # add the parent to the list
-            # recursive call to get all the parents of the parent
-            parents += get_all_parents_from_main_label(parents, taxonomy)
-            return parents
-        else:
-            return []
+    if label is None:
+        return None
+    if label in taxonomy[0].values:
+        # get the corresponding parent in column 0
+        parents = list(taxonomy[taxonomy[0] == label][2])
+        # add the parent to the list
+        # recursive call to get all the parents of the parent
+        parents += get_all_parents_from_main_label(parents, taxonomy)
+        return parents
+    else:
+        return []
 
 def get_all_relations(word, df_yago):
     # replace the space by "_" in the word
@@ -116,7 +118,12 @@ def word2id(word):
 
 
 def get_graph(word_to_encode):
-    main_labels = get_all_parents_from_main_label(get_main_label(word_to_encode, df), taxonomy)
+    main_label = get_main_label(word_to_encode, df)
+    if main_label is None:
+        root_node = RootNode(txt=word_to_encode)
+        graph = Graph(nodes=[root_node], root=root_node)
+        return graph
+    main_labels = get_all_parents_from_main_label(main_label, taxonomy)
     relations = get_all_relations(word_to_encode, df_facts)
     main_concept = set(get_words_and_derived(main_labels, False))
     leaf_nodes = set(get_words_and_derived(main_labels + relations, True)) & concepts
